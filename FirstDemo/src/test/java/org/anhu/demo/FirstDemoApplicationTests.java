353a887b14f60ba86e9e7e0e585bdc5dce6740a5
anneke.huijsmans <anneke.huijsmans@FWIN-07.internal.fourtress.nl>
2018-07-18 13:37:51 +0200

    added a smartcard reader from usb and a not yet working test for the consumer

--- FirstDemo/src/test/java/org/anhu/demo/FirstDemoApplicationTests.java
+++ FirstDemo/src/test/java/org/anhu/demo/FirstDemoApplicationTests.java
@@ -4,6 +4,7 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.web.client.RestTemplate;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
@@ -11,6 +12,16 @@ public class FirstDemoApplicationTests {
 
 	@Test
 	public void contextLoads() {
+		FirstDemoApplication demo = new FirstDemoApplication();
+
+		final String uri = "http://localhost:8080/abc";
+
+		Message message = new Message(1, 2);
+
+		RestTemplate restTemplate = new RestTemplate();
+		Message result = restTemplate.postForObject(uri, message, Message.class);
+
+		System.out.println(result);
 	}
 
 }
